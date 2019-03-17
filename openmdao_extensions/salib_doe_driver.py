8929ad281055c46b741742bdcc41f69e32a75297
Rémi Lafage <remi.lafage@onera.fr>
2019-03-17 13:36:32 +0100

    upgrade SALib 1.3.3

--- openmdao_extensions/salib_doe_driver.py
+++ openmdao_extensions/salib_doe_driver.py
@@ -20,7 +20,7 @@ except ImportError:
 
 class SalibMorrisDOEGenerator(DOEGenerator):
 
-    def __init__(self, n_trajs=100, n_levels=4, grid_step_size=2):
+    def __init__(self, n_trajs=2, n_levels=4):
         super(SalibMorrisDOEGenerator, self).__init__()
 
         if SALIB_NOT_INSTALLED:
@@ -31,8 +31,6 @@ class SalibMorrisDOEGenerator(DOEGenerator):
         self.n_trajs = n_trajs
         # number of grid levels
         self.n_levels = n_levels
-        # grid jump size
-        self.grid_step_size = grid_step_size        
 
     def __call__(self, design_vars, model=None):
         bounds=[]
@@ -61,7 +59,7 @@ class SalibMorrisDOEGenerator(DOEGenerator):
         self._pb = {'num_vars': len(names), 
                     'names': names, 
                     'bounds': bounds, 'groups': None}
-        cases = ms.sample(self._pb, self.n_trajs, self.n_levels, self.grid_step_size)
+        cases = ms.sample(self._pb, self.n_trajs, self.n_levels)
         sample = []
         for i in range(cases.shape[0]):
             j=0
@@ -83,17 +81,14 @@ class SalibMorrisDOEDriver(DOEDriver):
             raise RuntimeError('SALib library is not installed. \
                                 cf. https://salib.readthedocs.io/en/latest/getting-started.html')
 
-        self.options.declare('n_trajs', types=int, default=100,
+        self.options.declare('n_trajs', types=int, default=2,
                              desc='number of trajectories to apply morris method')
         self.options.declare('n_levels', types=int, default=4,
                              desc='number of grid levels')
-        self.options.declare('grid_step_size', types=int , default=2,
-                             desc='grid jump size')
         self.options.update(kwargs)
         n_trajs = self.options['n_trajs']
         n_levels = self.options['n_levels']
-        grid_step_size = self.options['grid_step_size']
-        self.options['generator'] = SalibMorrisDOEGenerator(n_trajs, n_levels, grid_step_size)
+        self.options['generator'] = SalibMorrisDOEGenerator(n_trajs, n_levels)
 
 class SalibDoeDriver(SalibMorrisDOEDriver):
     """
@@ -101,6 +96,58 @@ class SalibDoeDriver(SalibMorrisDOEDriver):
     """
     def __init__(self, **kwargs):
         super(SalibDoeDriver, self).__init__()
-        warn_deprecation("'SalibDoeDriver' is deprecated"
-                         "; use 'SalibMorrisDOEDriver' instead.")
+        warn_deprecation("'SalibDoeDriver' is deprecated; "
+                         "use 'SalibMorrisDOEDriver' instead.")
 
+# class SalibSobolDOEGenerator(DOEGenerator):
+
+#     def __init__(self, ):
+#         super(SalibSobolDOEGenerator, self).__init__()
+
+#         if SALIB_NOT_INSTALLED:
+#             raise RuntimeError('SALib library is not installed. \
+#                                 cf. https://salib.readthedocs.io/en/latest/getting-started.html')
+
+#         # number of trajectories to apply morris method
+#         self.n_trajs = n_trajs
+#         # number of grid levels
+#         self.n_levels = n_levels
+#         # grid jump size
+#         self.grid_step_size = grid_step_size        
+
+#     def __call__(self, design_vars, model=None):
+#         bounds=[]
+#         names=[]
+#         for name, meta in iteritems(design_vars):
+#             size = meta['size']
+#             meta_low = meta['lower']
+#             meta_high = meta['upper']
+#             for j in range(size):
+#                 if isinstance(meta_low, np.ndarray):
+#                     p_low = meta_low[j]
+#                 else:
+#                     p_low = meta_low
+
+#                 if isinstance(meta_high, np.ndarray):
+#                     p_high = meta_high[j]
+#                 else:
+#                     p_high = meta_high
+                    
+#                 display_name = name.split('.')[-1]
+#                 if size>1:
+#                     display_name += str(j)
+#                 names.append(display_name)
+#                 bounds.append((p_low, p_high))
+
+#         self._pb = {'num_vars': len(names), 
+#                     'names': names, 
+#                     'bounds': bounds, 'groups': None}
+#         cases = ms.sample(self._pb, self.n_trajs, self.n_levels, self.grid_step_size)
+#         sample = []
+#         for i in range(cases.shape[0]):
+#             j=0
+#             for name, meta in iteritems(design_vars):
+#                 size = meta['size']
+#                 sample.append((name, cases[i, j:j + size]))
+#                 j += size
+#             yield sample
--- openmdao_extensions/tests/test_salib_doe_driver.py
+++ openmdao_extensions/tests/test_salib_doe_driver.py
@@ -1,55 +1,66 @@
 import os
 import unittest
+from six import itervalues
+import tempfile
 from openmdao.api import IndepVarComp, Problem, Group, SqliteRecorder, CaseReader, DOEDriver, FullFactorialGenerator
 from openmdao.test_suite.components.sellar import SellarProblem
 from openmdao_extensions.salib_doe_driver import SalibMorrisDOEDriver, SalibMorrisDOEGenerator, SalibDoeDriver
 from openmdao_extensions.salib_doe_driver import SALIB_NOT_INSTALLED
-
+from openmdao.utils.assert_utils import assert_warning
 class TestSalibDoeDriver(unittest.TestCase):
 
-    def setUp(self):
-        self.pb = SellarProblem()        
-        self.case_recorder_filename = 'test_salib_doe_driver.sqlite'
-        self.recorder = SqliteRecorder(self.case_recorder_filename)
         
-    def tearDown(self):
-        if os.path.exists(self.case_recorder_filename):
-            os.remove(self.case_recorder_filename)
         
     @unittest.skipIf(SALIB_NOT_INSTALLED, 'SALib library is not installed')
     def test_salib_doe_driver(self):
-        pb = self.pb
-        pb.driver = SalibMorrisDOEDriver()
-        pb.driver.add_recorder(self.recorder)
+        pb = SellarProblem()        
+        case_recorder_filename = "test_salib_doe_driver.sqlite"
+        recorder = SqliteRecorder(case_recorder_filename)
+        nt = 4
+        pb.driver = SalibMorrisDOEDriver(n_trajs=nt)
+        pb.driver.add_recorder(recorder)
         pb.setup()
-        self.pb.run_driver()
-        self.pb.cleanup()
-        assert os.path.exists(self.case_recorder_filename)
-        reader = CaseReader(self.case_recorder_filename)
+        pb.run_driver()
+        pb.cleanup()
+
+        assert os.path.exists(case_recorder_filename)
+        reader = CaseReader(case_recorder_filename)
         cases = reader.list_cases('driver')
-        self.assertEqual(len(cases), 8)
-        for i in range(len(cases)):
-            case = reader.get_case(cases[i])
+        os.remove(case_recorder_filename)
+        n = sum(data['size'] for data in itervalues(pb.model.get_design_vars()))
+        self.assertEqual(len(cases), (n+1)*nt)
+        # for i in range(len(cases)):
+        #     case = reader.get_case(cases[i])
+        #     print(case)
 
     @unittest.skipIf(SALIB_NOT_INSTALLED, 'SALib library is not installed')
-    def test_doe_run(self): 
-        # create a list of DOE cases
-        pb = self.pb
-        pb.driver = DOEDriver(SalibMorrisDOEGenerator())
-        pb.driver.add_recorder(self.recorder)
+    def test_doe_generator(self): 
+        pb = SellarProblem()        
+        case_recorder_filename = "test_doe_generator.sqlite"
+        recorder = SqliteRecorder(case_recorder_filename)
+        nt = 5
+        pb.driver = DOEDriver(SalibMorrisDOEGenerator(n_trajs=nt))
+        pb.driver.add_recorder(recorder)
         pb.setup()
+        pb.run_driver()
+        pb.cleanup()
 
-        self.pb.run_driver()
-        self.pb.cleanup()
-        assert os.path.exists(self.case_recorder_filename)
-        reader = CaseReader(self.case_recorder_filename)
+        assert os.path.exists(case_recorder_filename)
+        reader = CaseReader(case_recorder_filename)
         cases = reader.list_cases('driver')
-        self.assertEqual(len(cases), 8)
-        for i in range(len(cases)):
-            case = reader.get_case(cases[i])
+        os.remove(case_recorder_filename)
+        n = sum(data['size'] for data in itervalues(pb.model.get_design_vars()))
+        self.assertEqual(len(cases), (n+1)*nt)
+        # for i in range(len(cases)):
+        #     case = reader.get_case(cases[i])
+        #     print(case)
 
+    @unittest.skipIf(SALIB_NOT_INSTALLED, 'SALib library is not installed')
     def test_deprecated(self):
-        driver = SalibDoeDriver()
+        msg = "'SalibDoeDriver' is deprecated; " \
+                "use 'SalibMorrisDOEDriver' instead."
+        with assert_warning(DeprecationWarning, msg):
+            SalibDoeDriver()
 
 if __name__ == '__main__':
     unittest.main()
