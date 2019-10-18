a21fef0bfce8c9d6884bdc89a0c064c694229b49
Rémi Lafage <remi.lafage@onera.fr>
2019-10-18 11:31:26 +0200

    black pass, remove unused import

--- openmdao_extensions/salib_doe_driver.py
+++ openmdao_extensions/salib_doe_driver.py
@@ -2,41 +2,40 @@
 Driver for running model on design of experiments cases using Salib sampling methods
 """
 from __future__ import print_function
-from six import itervalues, iteritems, reraise
+from six import iteritems
 from six.moves import range
 
 import numpy as np
 
-from openmdao.api import DOEDriver, ListGenerator, OptionsDictionary
-from openmdao.core.driver import Driver, RecordingDebugging
+from openmdao.api import DOEDriver, OptionsDictionary
 from openmdao.drivers.doe_generators import DOEGenerator
-from openmdao.utils.general_utils import warn_deprecation
 
 SALIB_NOT_INSTALLED = False
 try:
     from SALib.sample import morris as ms
     from SALib.sample import saltelli
-    from SALib.analyze import sobol
 except ImportError:
     SALIB_NOT_INSTALLED = True
 
-class SalibDOEGenerator(DOEGenerator):
 
+class SalibDOEGenerator(DOEGenerator):
     def __init__(self):
         if SALIB_NOT_INSTALLED:
-            raise RuntimeError('SALib library is not installed. \
-                                cf. https://salib.readthedocs.io/en/latest/getting-started.html')
-        self._cases = []
+            raise RuntimeError(
+                "SALib library is not installed. \
+                                cf. https://salib.readthedocs.io/en/latest/getting-started.html"
+            )
+        self._cases = np.array([])
         self._pb = None
         self.called = False
 
     def __call__(self, design_vars, model=None):
-        bounds=[]
-        names=[]
+        bounds = []
+        names = []
         for name, meta in iteritems(design_vars):
-            size = meta['size']
-            meta_low = meta['lower']
-            meta_high = meta['upper']
+            size = meta["size"]
+            meta_low = meta["lower"]
+            meta_high = meta["upper"]
             for j in range(size):
                 if isinstance(meta_low, np.ndarray):
                     p_low = meta_low[j]
@@ -47,24 +46,27 @@ class SalibDOEGenerator(DOEGenerator):
                     p_high = meta_high[j]
                 else:
                     p_high = meta_high
-                    
-                display_name = name.split('.')[-1]
-                if size>1:
+
+                display_name = name.split(".")[-1]
+                if size > 1:
                     display_name += str(j)
                 names.append(display_name)
                 bounds.append((p_low, p_high))
 
-        self._pb = {'num_vars': len(names), 
-                    'names': names, 
-                    'bounds': bounds, 'groups': None}
+        self._pb = {
+            "num_vars": len(names),
+            "names": names,
+            "bounds": bounds,
+            "groups": None,
+        }
         self._compute_cases()
         self.called = True
         sample = []
         for i in range(self._cases.shape[0]):
-            j=0
+            j = 0
             for name, meta in iteritems(design_vars):
-                size = meta['size']
-                sample.append((name, self._cases[i, j:j + size]))
+                size = meta["size"]
+                sample.append((name, self._cases[i, j : j + size]))
                 j += size
             yield sample
 
@@ -78,11 +80,13 @@ class SalibDOEGenerator(DOEGenerator):
 
     def get_salib_problem(self):
         if not self.called:
-            raise RuntimeError("Have to run the driver before getting the SALib problem")
+            raise RuntimeError(
+                "Have to run the driver before getting the SALib problem"
+            )
         return self._pb
 
-class SalibMorrisDOEGenerator(SalibDOEGenerator):
 
+class SalibMorrisDOEGenerator(SalibDOEGenerator):
     def __init__(self, n_trajs=2, n_levels=4):
         super(SalibMorrisDOEGenerator, self).__init__()
         # number of trajectories to apply morris method
@@ -93,8 +97,8 @@ class SalibMorrisDOEGenerator(SalibDOEGenerator):
     def _compute_cases(self):
         self._cases = ms.sample(self._pb, self.n_trajs, self.n_levels)
 
-class SalibSobolDOEGenerator(SalibDOEGenerator):
 
+class SalibSobolDOEGenerator(SalibDOEGenerator):
     def __init__(self, n_samples=2, calc_second_order=True):
         super(SalibSobolDOEGenerator, self).__init__()
         # number of trajectories to apply morris method
@@ -105,52 +109,77 @@ class SalibSobolDOEGenerator(SalibDOEGenerator):
     def _compute_cases(self):
         self._cases = saltelli.sample(self._pb, self.n_samples, self.calc_second_order)
 
+
 class SalibDOEDriver(DOEDriver):
     """
     Baseclass for SALib design-of-experiments Drivers
     """
+
     def __init__(self, **kwargs):
         super(SalibDOEDriver, self).__init__()
 
         if SALIB_NOT_INSTALLED:
-            raise RuntimeError('SALib library is not installed. \
-                                cf. https://salib.readthedocs.io/en/latest/getting-started.html')
+            raise RuntimeError(
+                "SALib library is not installed. \
+                                cf. https://salib.readthedocs.io/en/latest/getting-started.html"
+            )
 
-        self.options.declare('sa_method_name', default='Morris', values=['Morris', 'Sobol'],
-                             desc='either Morris or Sobol')
-        self.options.declare('sa_doe_options', types=dict, default={},
-                             desc='options for given SMT sensitivity analysis method')
+        self.options.declare(
+            "sa_method_name",
+            default="Morris",
+            values=["Morris", "Sobol"],
+            desc="either Morris or Sobol",
+        )
+        self.options.declare(
+            "sa_doe_options",
+            types=dict,
+            default={},
+            desc="options for given SMT sensitivity analysis method",
+        )
         self.options.update(kwargs)
 
         self.sa_settings = OptionsDictionary()
-        if self.options['sa_method_name'] == 'Morris':
-            self.sa_settings.declare('n_trajs', types=int, default=2,
-                                             desc='number of trajectories to apply morris method')
-            self.sa_settings.declare('n_levels', types=int, default=4,
-                                             desc='number of grid levels')
-            self.sa_settings.update(self.options['sa_doe_options'])
-            n_trajs = self.sa_settings['n_trajs']
-            n_levels = self.sa_settings['n_levels']
-            self.options['generator'] = SalibMorrisDOEGenerator(n_trajs, n_levels)
-        elif self.options['sa_method_name'] == 'Sobol':
-            self.sa_settings.declare('n_samples', types=int, default=2,
-                                     desc='number of samples to generate')
-            self.sa_settings.declare('calc_second_order', types=bool, default=True,
-                                     desc='calculate second-order sensitivities ')
-            self.sa_settings.update(self.options['sa_doe_options'])
-            n_samples = self.sa_settings['n_samples']
-            calc_snd = self.sa_settings['calc_second_order']
-            self.options['generator'] = SalibSobolDOEGenerator(n_samples, calc_snd)
+        if self.options["sa_method_name"] == "Morris":
+            self.sa_settings.declare(
+                "n_trajs",
+                types=int,
+                default=2,
+                desc="number of trajectories to apply morris method",
+            )
+            self.sa_settings.declare(
+                "n_levels", types=int, default=4, desc="number of grid levels"
+            )
+            self.sa_settings.update(self.options["sa_doe_options"])
+            n_trajs = self.sa_settings["n_trajs"]
+            n_levels = self.sa_settings["n_levels"]
+            self.options["generator"] = SalibMorrisDOEGenerator(n_trajs, n_levels)
+        elif self.options["sa_method_name"] == "Sobol":
+            self.sa_settings.declare(
+                "n_samples", types=int, default=2, desc="number of samples to generate"
+            )
+            self.sa_settings.declare(
+                "calc_second_order",
+                types=bool,
+                default=True,
+                desc="calculate second-order sensitivities ",
+            )
+            self.sa_settings.update(self.options["sa_doe_options"])
+            n_samples = self.sa_settings["n_samples"]
+            calc_snd = self.sa_settings["calc_second_order"]
+            self.options["generator"] = SalibSobolDOEGenerator(n_samples, calc_snd)
         else:
-            raise RuntimeError("Bad sensitivity analysis method name '{}'".format(self.options['sa_method_name']))
+            raise RuntimeError(
+                "Bad sensitivity analysis method name '{}'".format(
+                    self.options["sa_method_name"]
+                )
+            )
 
     def _set_name(self):
-        self._name = 'SALib_DOE_'+self.options['sa_method_name']
+        self._name = "SALib_DOE_" + self.options["sa_method_name"]
 
     def get_cases(self):
-        return self.options['generator'].get_cases()
+        return self.options["generator"].get_cases()
 
     def get_salib_problem(self):
-        return self.options['generator'].get_salib_problem()
-
+        return self.options["generator"].get_salib_problem()
 
--- openmdao_extensions/smt_doe_driver.py
+++ openmdao_extensions/smt_doe_driver.py
@@ -2,28 +2,30 @@
 Driver for running model on design of experiments cases using SMT sampling methods
 """
 from __future__ import print_function
-from six import itervalues, iteritems, reraise
+from six import iteritems
 import numpy as np
 
-from openmdao.api import DOEDriver, ListGenerator
-from openmdao.core.driver import Driver, RecordingDebugging
+from openmdao.api import DOEDriver
 from openmdao.drivers.doe_generators import DOEGenerator
 from openmdao.utils.general_utils import warn_deprecation
 
 SMT_NOT_INSTALLED = False
 try:
     from smt.sampling_methods import FullFactorial, LHS, Random
-except:
+except ImportError:
     SMT_NOT_INSTALLED = False
 
-_sampling_methods = {'FullFactorial': FullFactorial, 'LHS': LHS, 'Random': Random}
-class SmtDOEGenerator(DOEGenerator):
+_sampling_methods = {"FullFactorial": FullFactorial, "LHS": LHS, "Random": Random}
 
+
+class SmtDOEGenerator(DOEGenerator):
     def __init__(self, sampling_method_name, n_cases, **kwargs):
         super(SmtDOEGenerator, self).__init__()
 
         if SMT_NOT_INSTALLED:
-            raise RuntimeError('SMT library is not installed. cf. https://https://smt.readthedocs.io/en/latest')
+            raise RuntimeError(
+                "SMT library is not installed. cf. https://https://smt.readthedocs.io/en/latest"
+            )
 
         # number of trajectories to apply morris method
         self.sampling_method_name = sampling_method_name
@@ -33,11 +35,11 @@ class SmtDOEGenerator(DOEGenerator):
         self.sampling_method_opts = kwargs
 
     def __call__(self, design_vars, model=None):
-        xlimits=[]
+        xlimits = []
         for name, meta in iteritems(design_vars):
-            size = meta['size']
-            meta_low = meta['lower']
-            meta_high = meta['upper']
+            size = meta["size"]
+            meta_low = meta["lower"]
+            meta_high = meta["upper"]
             for j in range(size):
                 if isinstance(meta_low, np.ndarray):
                     p_low = meta_low[j]
@@ -51,52 +53,67 @@ class SmtDOEGenerator(DOEGenerator):
 
                 xlimits.append((p_low, p_high))
 
-        sampling = _sampling_methods[self.sampling_method_name](xlimits=np.array(xlimits), **(self.sampling_method_opts))
+        sampling = _sampling_methods[self.sampling_method_name](
+            xlimits=np.array(xlimits), **(self.sampling_method_opts)
+        )
         cases = sampling(self.n_cases)
         sample = []
         for i in range(cases.shape[0]):
-            j=0
+            j = 0
             for name, meta in iteritems(design_vars):
-                size = meta['size']
-                sample.append((name, cases[i, j:j + size]))
+                size = meta["size"]
+                sample.append((name, cases[i, j : j + size]))
                 j += size
             yield sample
+
 
 class SmtDOEDriver(DOEDriver):
     """
     Baseclass for SMT design-of-experiments Drivers 
     """
+
     def __init__(self, **kwargs):
         super(SmtDOEDriver, self).__init__()
 
         if SMT_NOT_INSTALLED:
-            raise RuntimeError('SMT library is not installed. cf. https://https://smt.readthedocs.io/en/latest')
+            raise RuntimeError(
+                "SMT library is not installed. cf. https://https://smt.readthedocs.io/en/latest"
+            )
 
-        self.options.declare('sampling_method_name', types=str, default='LHS',
-                             desc='either LHS, FullFactorial or Ramdom')
-        self.options.declare('n_cases', types=int, default=2,
-                             desc='number of sample to generate')
-        self.options.declare('sampling_method_options', types=dict, default={},
-                             desc='options for given SMT sampling method')
+        self.options.declare(
+            "sampling_method_name",
+            types=str,
+            default="LHS",
+            desc="either LHS, FullFactorial or Ramdom",
+        )
+        self.options.declare(
+            "n_cases", types=int, default=2, desc="number of sample to generate"
+        )
+        self.options.declare(
+            "sampling_method_options",
+            types=dict,
+            default={},
+            desc="options for given SMT sampling method",
+        )
 
         self.options.update(kwargs)
-        name = self.options['sampling_method_name']
-        n_cases = self.options['n_cases']
-        opts = self.options['sampling_method_options']
-        self.options['generator'] = SmtDOEGenerator(sampling_method_name=name, n_cases=n_cases, **opts)
+        name = self.options["sampling_method_name"]
+        n_cases = self.options["n_cases"]
+        opts = self.options["sampling_method_options"]
+        self.options["generator"] = SmtDOEGenerator(
+            sampling_method_name=name, n_cases=n_cases, **opts
+        )
 
     def _set_name(self):
-        self._name = 'SMT_DOE_'+self.options['sampling_method_name']
+        self._name = "SMT_DOE_" + self.options["sampling_method_name"]
+
 
 class SmtDoeDriver(SmtDOEDriver):
     """
     Deprecated. Use SmtDOEDriver  
     """
+
     def __init__(self, **kwargs):
         super(SmtDoeDriver, self).__init__(**kwargs)
-        warn_deprecation("'SmtDoeDriver' is deprecated; "
-                         "use 'SmtDOEDriver' instead.")
-
-
-
+        warn_deprecation("'SmtDoeDriver' is deprecated; " "use 'SmtDOEDriver' instead.")
 
