22a1c9688ad9267a86823844e07e3e74c1d61262
relf <remi.lafage@onera.fr>
2020-03-06 18:29:06 +0100

    bump 1.0.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 CLASSIFIERS = """
-Development Status :: 4 - Beta
+Development Status :: 5 - Production/Stable
 Intended Audience :: Science/Research
 Intended Audience :: Developers
 License :: OSI Approved :: Apache Software License
@@ -29,7 +29,7 @@ Operating System :: MacOS
 
 metadata = dict(
     name="openmdao_extensions",
-    version="0.4.0",
+    version="1.0.0",
     description="Additional solvers and drivers for OpenMDAO framework",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -39,7 +39,7 @@ metadata = dict(
     classifiers=[_f for _f in CLASSIFIERS.split("\n") if _f],
     packages=["openmdao_extensions"],
     install_requires=["openmdao"],
-    python_requires="!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*",
+    python_requires=">=3.6",
     zip_safe=True,
     url="https://github.com/OneraHub/opendmao_extensions",
     download_url="https://github.com/OneraHub/openmdao_extensions/releases",
